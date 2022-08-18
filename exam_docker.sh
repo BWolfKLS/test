592c92f6b29cfcdeca4c300492611ba40d74db1c
BWolfKLS <106620029+BWolfKLS@users.noreply.github.com>
2022-08-18 20:43:00 +0300

    Update exam_docker.sh

--- exam_docker.sh
+++ exam_docker.sh
@@ -5,10 +5,7 @@ CONTAINER=bw-study_app
 
 if [ "(docker ps -q -f name=$CONTAINER)" ]; then
 	docker stop $CONTAINER ; docker rm $CONTAINER
-	docker run -d -p 9500:80 --name bw-study_app bwolf84/study:$CIRCLE_SHA1
-	echo "done 1 cicle docker"
-else 	docker run -d -p 9500:80 --name bw-study_app bwolf84/study:$CIRCLE_SHA1
-	echo "done 2 cicle docker"
+else echo "done 2 cicle docker"
 fi
 
 
